baaa8f4dc2712bdfbc3d22fa39e5adfa626f3893
Timothee Groleau <tg@timotheegroleau.com>
2013-05-16 10:16:43 +0800

    algorithm is complete, phew

--- fun/funny_sig.js
+++ fun/funny_sig.js
@@ -1,7 +1,11 @@
 /*jshint laxbreak:true */
 
+// ================================
+// Utility functions
+// ================================
+
 function pad(num, size) {
-    var s = "00000000" + num;
+    var s = "00000000" + num; // a char is maximum 8 bits, so we harcode 8 zeros for convenience
     return s.substr(s.length-size);
 }
 
@@ -17,9 +21,27 @@ function format(template /*, args */) {
 	return template;
 }
 
-var tpl_sig_backward = "n=?;s=\"\";do{s=String.fromCharCode((n&?)+?)+s}while(n>>>=?)";
-var tpl_sig_forward = "n=?;s=\"\";do{s+=String.fromCharCode((n&?)+?)}while(n>>>=?)";
 
+
+// ================================
+// Signature functions
+//
+// SigWordOrder means that the compressed-range characters in the computed number are 
+// in the same order as the ascii-based string
+// 
+// for example: using "tim" as word
+// the signature in word-order will be 2820, or in binary: ['1011', '0000', '0100'], representing ['t', 'i', 'm']
+// the signature in reverse-order will be 1035, or in binary: ['0100', '0000', '1011'], representing ['m', 'i', 't']
+//
+// The order matters because the number is processed sequentially and the last character processed MUST NOT be falsy
+// that means the last character evaluated cannot be zero
+//
+// if only one end of the word is zero, the order is determined easily
+// if both ends of the word are zero (e.g. "amelia"), then we must change the base so 'a' is 1 instead of 0 in the new base
+// ================================
+
+var tpl_sig_reverse_order  = "eval('n=?;s=\"\";do{s+=String.fromCharCode((n&?)+?)}while(n>>>=?)')";
+var tpl_sig_word_order = "eval('n=?;s=\"\";do{s=String.fromCharCode((n&?)+?)+s}while(n>>>=?)')";
 
 function _getSig(sig_template, word, bits_per_char, base_ascii)
 {
@@ -39,25 +61,29 @@ function _getSig(sig_template, word, bits_per_char, base_ascii)
 		);
 }
 
-function getSigBackward(word, bits_per_char, base_ascii)
+function getSigWordOrder(word, bits_per_char, base_ascii)
 {
-	return _getSig(tpl_sig_backward, word, bits_per_char, base_ascii);
+	return _getSig(tpl_sig_word_order, word, bits_per_char, base_ascii);
 }
 
-function getSigForward(word, bits_per_char, base_ascii)
+function getSigReverseOrder(word, bits_per_char, base_ascii)
 {
-	return _getSig(tpl_sig_forward, word.split('').reverse().join(''), bits_per_char, base_ascii);
+	return _getSig(tpl_sig_reverse_order, word.split('').reverse().join(''), bits_per_char, base_ascii);
 }
 
 
 var ERR_TOO_LONG = "?: Unable to compute funny sig, character range too broad for word length";
 
 
+// ================================
+// Main algorithm
+// ================================
+
 function funnysig(word)
 {
 	var TOTAL_BITS = 32, idx;
 
-	// find lowest and highest ascii chars
+	// find lowest and highest ascii codes
 	var min_ascii = Infinity, max_ascii = -Infinity;
 	for (idx=word.length; idx--;)
 	{
@@ -66,49 +92,63 @@ function funnysig(word)
 		if (code > max_ascii) max_ascii = code;
 	}
 
-	// we try to get min_ascii translated to become index 0 in our mini range
+	// also get ascii codes for first and last chars
+	var first_ascii = word.charCodeAt(0);
+	var last_ascii = word.charCodeAt(word.length - 1);
 
-	// first we check if we are on an exact range
+	// if both ends of the word are min_ascii (that means will get transposed to char 0 (==falsy)),
+	// then we NEED to decrease min_ascii by 1, so the chars will get transposed to 1 and be truthy
+	// e.g. "amelia"
+	if (first_ascii === min_ascii && last_ascii === last_ascii)
+	{
+		min_ascii -= 1;
+	}
+
 	var required_range = max_ascii - min_ascii + 1;
 	var bits_per_char = Math.ceil( Math.log(required_range) / Math.log(2) );
+	var num_chars_allowed = Math.floor(TOTAL_BITS / bits_per_char);
 
-	// exit early if this word cannot be managed
-	if (bits_per_char * word.length > TOTAL_BITS)
+	if (word.length > num_chars_allowed + 1)
 	{
+		// a full extra character is needed, impossible to match
+		throw format(ERR_TOO_LONG, word);
+	}
+	
+	if (word.length > num_chars_allowed)
+	{
+		// we are only missing a single character!
+		// see if we can make use of the extra bits to squeeze it in!
+
+		var extra_bits = TOTAL_BITS % bits_per_char;
+		var first_char_binary = (first_ascii - min_ascii).toString(2);
+		var last_char_binary = (last_ascii - min_ascii).toString(2);
+
+		if (first_char_binary.length <= extra_bits && first_char_binary !== '0')
+		{
+			return getSigWordOrder(word, bits_per_char, min_ascii);
+		}
+		
+		if (last_char_binary.length <= extra_bits && last_char_binary !== '0')
+		{
+			return getSigReverseOrder(word, bits_per_char, min_ascii);
+		}
+
 		throw format(ERR_TOO_LONG, word);
 	}
 
-	// now check edge cases:
-	// we need to worry about cases where the index 0 is either first or last
-	if (word.charCodeAt(word.length - 1) == min_ascii)
+	// if we reach here, we can always fit the word, yeah!
+	if (first_ascii === min_ascii)
 	{
-		if (word.charCodeAt(0) != min_ascii)
-		{
-			return getSigBackward(word, bits_per_char, min_ascii);
-		}
-		
-		// we have zeros on both end (e.g. "amelia")
-		// that means the base needs to be 1 rather than zero
-		min_ascii -= 1;
-
-		// to accomodate the extra 1 offset
-		// we need to check if the current range can accomodate a +1
-		// or if we need an extra bit :(
-		if (isPowerOfTwo(required_range))
-		{
-			bits_per_char += 1;
-
-			// check if can still fit...
-			// we know the last character only require 1 bit to work!
-			if ((bits_per_char * (word.length - 1)) + 1 > TOTAL_BITS)
-			{
-				throw format(ERR_TOO_LONG, word);
-			}
-		}
+		return getSigReverseOrder(word, bits_per_char, min_ascii);
 	}
 
-	return getSigForward(word, bits_per_char, min_ascii);
+	return getSigWordOrder(word, bits_per_char, min_ascii);
 }
+
+
+// ================================
+// process starts here!
+// ================================
 
 if ( !process || !process.argv || !process.argv[2] )
 {
@@ -117,6 +157,16 @@ if ( !process || !process.argv || !process.argv[2] )
 }
 else
 {
-	var sig = funnysig( process.argv[2] );
-	console.log( process.argv[2] + ": " + sig + 'console.log(s) -> '  + eval(sig));
+	try
+	{
+		var sig = funnysig( process.argv[2] );
+	}
+	catch(e)
+	{
+		console.log(e);
+		process.exit(1);
+	}
+
+	console.log( process.argv[2] + ": " + sig + ' -> '  + eval(sig));
+	process.exit(0);
 }
